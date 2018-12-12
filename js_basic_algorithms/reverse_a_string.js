d3f22e7514e2bb053c341caf3f35573c4c8403fe
Jayfiled <jayfiled@gmail.com>
2018-12-12 23:47:34 +1100

    add exercises

---
+++ js_basic_algorithms/reverse_a_string.js
@@ -0,0 +1,17 @@
+/*
+Reverse the provided string.
+
+Your result must be a string.
+*/
+
+function reverseString(str) {
+    return str;
+  }
+  
+  reverseString("hello");
+
+// test cases:
+  reverseString("hello") // should return a string.
+  reverseString("hello") // should become "olleh".
+  reverseString("Howdy") // should become "ydwoH".
+  reverseString("Greetings from Earth") // should return "htraE morf sgniteerG".
