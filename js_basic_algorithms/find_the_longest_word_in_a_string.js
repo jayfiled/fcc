b53cc30a3db11a1303827addd6276d0e72ea96a2
Jayfiled <jayfiled@gmail.com>
2018-12-17 23:28:47 +1100

    solve longest word in string algorithm 3 ways

--- js_basic_algorithms/find_the_longest_word_in_a_string.js
+++ js_basic_algorithms/find_the_longest_word_in_a_string.js
@@ -1,23 +1,62 @@
 /* Return the length of the longest word in the provided sentence.
 
 Your response should be a number.
-*/
 
+
+// Understand
+    There is a sentence as an input.  Find out which word in the sentence is the longest.
+
+// Plan
+Make the sentence an array, with each word making up an element in the array
+Use reduce, compare the length of each element and save, or keep the one
+that is the longest in the accumulator. <-- I had trouble thinking about the acc as a number, which doesn't 
+have a length property.
+
+Do the same with map() 
+
+Use sort()
+
+*/
 function findLongestWordLength(str) {
-    return str.length;
+    let longWord = str.split(" ")
+    .reduce((longWord, eachWord) => { 
+        return eachWord.length > longWord ? eachWord.length : longWord
+    }, 0);
+
+    return longWord;
   }
   
   findLongestWordLength("The quick brown fox jumped over the lazy dog");
+ /*
 
-// Understand
-    // There is a sentence as an input.  Find out which word in the sentence is the longest.
+*/
+// Complete using the easiest method to me:
 
-// Plan
-  // Make the sentence an array, with each word making up an element in the array
+function findLongestWordLength(str) {
+        let newStr = str.split(" ");
+        
+        let longestWord = 0;
+        
+        newStr.map(num => {
+        if (num.length > longestWord)
+        longestWord = num.length;	
+    })
+    return longestWord;
+    
+      }
+      
+      findLongestWordLength("The quick brown fox jumped over the lazy dog");
 
-// Divide
+// Solved using sort
+
+function findLongestWordLength(str) {
+    return str.split(' ')
+        .sort((a,b) => a.length - b.length)
+        .pop().length;
+}
 
 
+findLongestWordLength("The quick brown fox jumped over the lazy dog")
 
 
 
