6f997ea3d14c588ecd71496c69c0b2a844fded38
Jayfiled <jayfiled@gmail.com>
2018-12-31 18:02:48 +1100

    update readme and add new challenge

---
+++ js_basic_algorithms/repeat_a_string_repeat_a_string.js
@@ -0,0 +1,21 @@
+/*
+Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number.
+*/
+
+function repeatStringNumTimes(str, num) {
+    // repeat after me
+    return str;
+  }
+  
+  repeatStringNumTimes("abc", 3);
+
+
+// Test cases
+repeatStringNumTimes("*", 3) // should return "***".
+repeatStringNumTimes("abc", 3) // should return "abcabcabc".
+repeatStringNumTimes("abc", 4) // should return "abcabcabcabc".
+repeatStringNumTimes("abc", 1) // should return "abc".
+repeatStringNumTimes("*", 8) // should return "********".
+repeatStringNumTimes("abc", -2) // should return "".
+
+//The built-in repeat()-method should not be used
