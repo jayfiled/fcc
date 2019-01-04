e7b28f174625cfddf497f37815b3b9f5ed1e72ad
Jayfiled <jayfiled@gmail.com>
2019-01-04 12:46:12 +1100

    solve algorithm to check if an input is a type of a scary ghost diet

--- js_basic_algorithms/boo_who.js
+++ js_basic_algorithms/boo_who.js
@@ -5,8 +5,7 @@ Boolean primitives are true and false.
 */
 
 function booWho(bool) {
-    // What is the new fad diet for ghost developers? The Boolean.
-    return bool;
+  return typeof bool === 'boolean' ? true : false;
   }
   
   booWho(null);
