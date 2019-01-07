81e441f79e41f723686c44449388c5270ad76601
Jayfiled <jayfiled@gmail.com>
2019-01-07 13:04:33 +1100

    work on uppercasing the first letter of a sentance algorithm

--- js_basic_algorithms/title_case_a_sentence.js
+++ js_basic_algorithms/title_case_a_sentence.js
@@ -5,7 +5,21 @@ For the purpose of this exercise, you should also capitalize connecting words li
 */
 
 function titleCase(str) {
-    return str;
+// split and make all lower case to avoid case issues
+
+const newStr = str.split(' ')
+.toLowerCase() // ['i'm', 'a', 'little', 'teapot']
+
+// make  the 0th element of each element uppercase by splicing out the 0th index into a separate var
+// then splice the rest of the indexed items to their own var
+// then return the concatenated vars
+
+.map(element => {
+  const firstLetter = element.splice(0,1).toUpperCase
+})      
+
+  console.log(result);
+    // return str;
   }
   
   titleCase("I'm a little tea pot");
@@ -13,6 +27,7 @@ function titleCase(str) {
   // split the string by it's spaces into an array
   // make all letters lowercase
   // uppercase the [0]th element of each string
+    // each element's [0]th element should be turned to uppercase and the element returned
   // join the array by removing spaces to convert to a string
   // return string 
 
